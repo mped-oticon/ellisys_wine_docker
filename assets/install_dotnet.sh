01047719ca6d0ae777dd28eea9d76e6cf7be5a5b
Guillaume Bailey <gubailey@google.com>
2023-05-03 15:09:21 +0200

    Update to Debian Bullseye and .Net 4.8

--- assets/install_dotnet.sh
+++ assets/install_dotnet.sh
@@ -2,7 +2,7 @@
 
 # See https://appdb.winehq.org/objectManager.php?sClass=version&iId=34219
 WINEDLLOVERRIDES="mscoree,mshtml=" wineboot --init
-winetricks --unattended --force dotnet461 corefonts
+winetricks --unattended --force dotnet48 corefonts
 chmod -R a+rxw "$WINEPREFIX"  # Ensure everybody can access newly installed stuff
 
 rm -rf /tmp/*wine*
